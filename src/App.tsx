7200bdaf4402db21d1b016061b9c7ac204e3cdcb
GabrielLineker <gabriel.lineker@estudante.ufjf.br>
2025-12-07 16:54:08 -0300

    Refatoração do app para facilitar a leitura e etendimento do código

--- src/App.tsx
+++ src/App.tsx
@@ -87,6 +87,126 @@ function App() {
     alert("Recompensa Pega!");
   };
 
+
+  
+
+  const renderizarConteudo = () => {
+    if (login.mostraLogin && !login.teveLogin) {
+      return <Login TemLogin={LoginCompleto} />;
+    }
+
+    if (deveMostrarFormularioAdicao) {
+      return (
+        <InterfaceExibicao>
+          <FormularioNovoCriminoso 
+            submeter={submeterNovoCriminoso} 
+            cancelaSubmeter={cancelarNovoCriminoso} 
+            ultimoId={listaAtualizada.length ? Math.max(...listaAtualizada.map(p => p.id)) : 0}
+          />
+        </InterfaceExibicao>
+      );
+    }
+
+    if (deveMostrarRemocao) {
+      return (
+        <InterfaceExibicao>
+          <RemoverCriminosoDoSistema 
+            lista={listaAtualizada}
+            onRemover={removerCriminoso}
+            voltarPrincipal={voltarPrincipal}
+          />
+        </InterfaceExibicao>
+      );
+    }
+
+    if (personagemSelecionadoFicha) {
+      return (
+        <InterfaceExibicao>
+          <Ficha
+            nome={personagemSelecionadoFicha.Nome}
+            subnome={personagemSelecionadoFicha.Subnome}
+            imagem={personagemSelecionadoFicha.Imagem}
+            nivelPerigo={personagemSelecionadoFicha.NivelPerigo}
+            status={personagemSelecionadoFicha.Status}
+            idade={personagemSelecionadoFicha.Idade}
+            dataNascimento={personagemSelecionadoFicha.DataDeNascimento}
+            recompensa={personagemSelecionadoFicha.Recompensa}
+            peso={personagemSelecionadoFicha.Peso}
+            altura={personagemSelecionadoFicha.Altura}
+            descricao={personagemSelecionadoFicha.Descricao}
+            crimes={personagemSelecionadoFicha.Crimes}
+            ultimaLocalizacao={personagemSelecionadoFicha.UltimaLocalizacao}
+            onVoltar={clickVoltar}
+            onPegarRecompensa={clickPegarRecompensa}
+          />
+        </InterfaceExibicao>
+      );
+    }
+
+    return (
+      <>
+        <div className="interface-procurados">
+          <h1 className="titulo-principal">PROCURADOS</h1>
+
+          <div className="container-busca">
+            <div className="interface-busca">
+              <input
+                type="text"
+                className="campo-texto"
+                placeholder="Buscar por nome..."
+                value={filtros.nome}
+                onChange={(texto) => setFiltros({...filtros, nome: texto.target.value})}
+              />
+              <select 
+                className="campo-select divisa-esquerda" 
+                value={filtros.estrela} 
+                onChange={(estrela) => setFiltros({...filtros, estrela: Number(estrela.target.value)})}
+              >
+                <option value={0}>🌟</option>
+                <option value={1}>⭐</option>
+                <option value={2}>⭐⭐</option>
+                <option value={3}>⭐⭐⭐</option>
+                <option value={4}>⭐⭐⭐⭐</option>
+                <option value={5}>⭐⭐⭐⭐⭐</option>
+              </select>
+
+              <select 
+                className="campo-select divisa-esquerda" 
+                value={filtros.status} 
+                onChange={(status) => setFiltros({...filtros, status: status.target.value})}
+              >
+                <option value="">Todos</option>
+                <option value="Foragido">Foragido</option>
+                <option value="Capturado">Capturado</option>
+                <option value="Morto">Morto</option>
+                <option value="Desconhecido">Desconhecido</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <InterfaceExibicao>
+          {listaFiltrada.map((personagem) => (
+            <div className="card" key={personagem.id}>
+              <Personagem
+                nome={personagem.Nome}
+                subnome={personagem.Subnome}
+                imagem={personagem.Imagem}
+                nivelPerigo={personagem.NivelPerigo}
+                status={personagem.Status}
+                idade={personagem.Idade}
+                dataNascimento={personagem.DataDeNascimento}
+                recompensa={personagem.Recompensa}
+                onVerFicha={() => clickVerFicha(personagem)}
+                TemLogin={login.teveLogin}
+                tipoAcesso={login.userInfo?.nivelAcesso || ""}
+              />
+            </div>
+          ))}
+        </InterfaceExibicao>
+      </>
+    );
+  };
+
   return (
     <div className="App">
       <Cabecalho 
@@ -98,112 +218,8 @@ function App() {
         onClickRemover={clickRemover}
         onClickVisualizar={voltarPrincipal}
       />
-      
-      {login.mostraLogin && !login.teveLogin? (
-          <Login TemLogin={LoginCompleto} />
-      )
-      :deveMostrarFormularioAdicao ? (
-            <InterfaceExibicao>
-                <FormularioNovoCriminoso 
-                    submeter={submeterNovoCriminoso} 
-                    cancelaSubmeter={cancelarNovoCriminoso} 
-                    ultimoId={listaAtualizada.length ? Math.max(...listaAtualizada.map(p => p.id)) : 0}
-                />
-            </InterfaceExibicao>
-
-        ) : deveMostrarRemocao ? (
-            <InterfaceExibicao>
-                <RemoverCriminosoDoSistema 
-                    lista={listaAtualizada}
-                    onRemover={removerCriminoso}
-                    voltarPrincipal={voltarPrincipal}
-                />
-            </InterfaceExibicao>
-
-        ) : personagemSelecionadoFicha ? (
-            <InterfaceExibicao>
-                <Ficha
-                    nome={personagemSelecionadoFicha!.Nome}
-                    subnome={personagemSelecionadoFicha!.Subnome}
-                    imagem={personagemSelecionadoFicha!.Imagem}
-                    nivelPerigo={personagemSelecionadoFicha!.NivelPerigo}
-                    status={personagemSelecionadoFicha!.Status}
-                    idade={personagemSelecionadoFicha!.Idade}
-                    dataNascimento={personagemSelecionadoFicha!.DataDeNascimento}
-                    recompensa={personagemSelecionadoFicha!.Recompensa}
-                    peso={personagemSelecionadoFicha!.Peso}
-                    altura={personagemSelecionadoFicha!.Altura}
-                    descricao={personagemSelecionadoFicha!.Descricao}
-                    crimes={personagemSelecionadoFicha!.Crimes}
-                    ultimaLocalizacao={personagemSelecionadoFicha!.UltimaLocalizacao}
-                    onVoltar={clickVoltar}
-                    onPegarRecompensa={clickPegarRecompensa}
-                />
-            </InterfaceExibicao>
-
-        ) : (
-            <>
-              <div className="interface-procurados">
-                <h1 className="titulo-principal">PROCURADOS</h1>
-
-                <div className="container-busca">
-                  <div className="interface-busca">
-                    <input
-                      type="text"
-                      className="campo-texto"
-                      placeholder="Buscar por nome..."
-                      value={filtros.nome}
-                      onChange={(texto) => setFiltros({...filtros, nome: texto.target.value})}
-                    />
-                     <select 
-                        className="campo-select divisa-esquerda" 
-                        value={filtros.estrela} 
-                        onChange={(estrela) => setFiltros({...filtros, estrela: Number(estrela.target.value)})}
-                    >
-                        <option value={0}>🌟</option>
-                        <option value={1}>⭐</option>
-                        <option value={2}>⭐⭐</option>
-                        <option value={3}>⭐⭐⭐</option>
-                        <option value={4}>⭐⭐⭐⭐</option>
-                        <option value={5}>⭐⭐⭐⭐⭐</option>
-                    </select>
-
-                    <select 
-                        className="campo-select divisa-esquerda" 
-                        value={filtros.status} 
-                        onChange={(status) => setFiltros({...filtros, status: status.target.value})}
-                    >
-                        <option value="">Todos</option>
-                        <option value="Foragido">Foragido</option>
-                        <option value="Capturado">Capturado</option>
-                        <option value="Morto">Morto</option>
-                        <option value="Desconhecido">Desconhecido</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-              <InterfaceExibicao>
-                {listaFiltrada.map((personagem) => (
-                  <div className="card" key={personagem.id}>
-                    <Personagem
-                      nome={personagem.Nome}
-                      subnome={personagem.Subnome}
-                      imagem={personagem.Imagem}
-                      nivelPerigo={personagem.NivelPerigo}
-                      status={personagem.Status}
-                      idade={personagem.Idade}
-                      dataNascimento={personagem.DataDeNascimento}
-                      recompensa={personagem.Recompensa}
-                      onVerFicha={() => clickVerFicha(personagem)}
-                      TemLogin={login.teveLogin}
-                      tipoAcesso={login.userInfo?.nivelAcesso || ""}
-                    />
-                  </div>
-                ))}
-              </InterfaceExibicao>
-            </>
-        )}
-      </div>
+      {renderizarConteudo()}
+    </div>
   );
 }
 
