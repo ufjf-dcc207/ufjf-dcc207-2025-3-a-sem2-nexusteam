143ae4605becaabf91d59d3920e4f4954727e9be
Gianlucca Paiva <gianluccapaiva@gmail.com>
2025-12-08 09:11:24 -0300

    correção da hospedagem do site

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ufjf-dcc207-2025-3-a-sem01-nexus-team/',
+  base: '/ufjf-dcc207-2025-3-a-sem2-nexusteam/',
 })
 
